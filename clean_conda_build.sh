c0efc9ae03dc1b290b6c2d0c96be143402a751a9
acwikla <acwikla@novelaneuro.com>
2020-04-06 12:38:48 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	setup.py

---
+++ clean_conda_build.sh
@@ -0,0 +1,9 @@
+echo "Extract archive"
+mkdir new_tar
+tar -xf test_release_4-0.1.016-py37_0.tar.bz2 -C ./new_tar || exit 1
+
+echo "Create new archive"
+cd new_tar || exit 1
+tar -cjvf test_release_4-0.1.016-py37_0.tar.bz2 --exclude=info/recipe/dir_to_exclude --exclude=info/recipe/test --exclude=*.sh info Lib || exit 1
+read _
+
--- conda_upload.sh
+++ conda_upload.sh
@@ -9,6 +9,14 @@ echo "Building conda package..."
 conda build . || exit 1
 export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 
+echo "Extracting conda package..."
+mv $CONDA_BUILD_PATH/linux-64/test_release_4-0.1.016-py37_0.tar.bz2  $CONDA_BUILD_PATH
+mkdir $CONDA_BUILD_PATH/new_tar
+tar -xf $CONDA_BUILD_PATH/test_release_4-0.1.016-py37_0.tar.bz2 -C $CONDA_BUILD_PATH/new_tar || exit 1
+
+echo "Creating new conda package excluding some folders..."
+tar -cjvf $CONDA_BUILD_PATH/linux-64/test_release_4-0.1.016-py37_0.tar.bz2 --exclude=info/recipe/dir_to_exclude --exclude=info/recipe/test --exclude=*.sh $CONDA_BUILD_PATH/new_dir/info $CONDA_BUILD_PATH/new_dir/Lib || exit 1
+
 echo "Converting conda package..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
 conda convert --platform linux-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
