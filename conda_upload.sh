75e69b3b4cfba3c5df918dcc5ff33b13f3d8c62c
acwikla <acwikla@novelaneuro.com>
2020-04-06 14:29:50 +0200

    Merge remote-tracking branch 'origin/master'

--- conda_upload.sh
+++ conda_upload.sh
@@ -19,9 +19,8 @@ mkdir ${CONDA_BUILD_PATH}/new_tar
 echo "Extracting conda package..."
 tar -xf ${CONDA_BUILD_PATH}/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 -C ${CONDA_BUILD_PATH}/new_tar || exit 1
 
-ls ${CONDA_BUILD_PATH}/new_tar
 echo "Creating new conda package without some files..."
-tar -cjvf ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 --exclude=info/recipe/dir_to_exclude --exclude=info/recipe/test --exclude=*.sh ${CONDA_BUILD_PATH}/new_tar/info ${CONDA_BUILD_PATH}/new_tar/Lib || exit 1
+tar -cjvf ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 --exclude=info/recipe/dir_to_exclude --exclude=info/recipe/test --exclude=*.sh ${CONDA_BUILD_PATH}/new_tar/info ${CONDA_BUILD_PATH}/new_tar/lib || exit 1
 
 echo "Converting conda package..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
