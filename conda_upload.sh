d136290e39f4c3343a6e542890ee23b680db15aa
acwikla <acwikla@novelaneuro.com>
2020-04-07 10:37:47 +0200

    Merge remote-tracking branch 'origin/master'

--- conda_upload.sh
+++ conda_upload.sh
@@ -25,7 +25,7 @@ echo "Listing new_tar/info/"
 ls ${CONDA_BUILD_PATH}/new_tar/info/
 
 echo "Creating new conda package without some files..."
-tar -cjvf ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 --exclude=info/recipe/dir_to_exclude --exclude=info/recipe/test --exclude='*.sh' ${CONDA_BUILD_PATH}/new_tar/info ${CONDA_BUILD_PATH}/new_tar/lib || exit 1
+tar -cjvf ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 --exclude=info/recipe/dir_to_exclude --exclude=info/recipe/test --exclude='*.sh' --exclude=*.gitignore --exclude=*.pytest_cache ${CONDA_BUILD_PATH}/new_tar/info ${CONDA_BUILD_PATH}/new_tar/lib || exit 1
 
 ls ${CONDA_BUILD_PATH}/linux-64/
 
